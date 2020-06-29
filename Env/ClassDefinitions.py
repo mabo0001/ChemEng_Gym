50c30b6a943daad14fb4da6f2854da289dc9bc0d
lollcat <44415540+lollcat@users.noreply.github.com>
2020-06-29 18:20:24 +0200

    Updating Env
    Remove old env. Replace with ChemSep hydrocarbons example + lots of debugging

--- Env/ClassDefinitions.py
+++ Env/ClassDefinitions.py
@@ -1,52 +1,47 @@
 import numpy as np
 
+
 class Stream:
-    def __init__(self, number, flows):
+    def __init__(self, number, flows, temperature, pressure):
         """
         :param number: stream number, starts from zero
         :param flows: stream flowrates in moles, numpy array
-        could maybe add temp pressure stuff
+        :param temperature: stream temperature
         """
         self.number = number
-        self.flows = flows
+        self.flows = list(flows)
+        self.temperature = temperature
+        self.pressure = pressure
+
 
 
 class State:
-    def __init__(self, feed_stream, max_streams, shuffle=True):
+    """
+    This state doesn't include a shuffler
+    For now the state includes temperature and pressure as added straight to the end of the stream vector
+    """
+    def __init__(self, feed_stream, max_streams):
         self.streams = [feed_stream]
         self.max_streams = max_streams
-        self.shuffle = shuffle
-        self.stream_state_mapper = np.arange(self.max_streams)
-        self.blank_state = np.zeros((self.max_streams, feed_stream.flows.size))
-        self.rng = np.random.default_rng()
-        self.state = self.create_state()
+        self.state = np.zeros((self.max_streams, len(feed_stream.flows) + 2))  # +2 is for T & P
+        self.create_state()
 
     def create_state(self):
-        """
-        Turns the list of streams into the state (np.array) of the streams in a random (but known) order,
-        0's for null steams
-        """
-        if self.shuffle is True:
-            self.rng.shuffle(self.stream_state_mapper)  # shuffle mapper
-        else:
-            pass  # keep stream mapper unshuffled
-        self.state = self.blank_state
-        # assign streams to random locations in the state
-        # can make the below loop quicker - for now write it out to make it clear
-        self.state[self.stream_state_mapper[0:self.n_streams]] = [stream.flows for stream in self.streams]
-        return self.state
+        self.state[0:self.n_streams] = [stream.flows + [stream.temperature, stream.pressure] for stream in self.streams]
 
     def update_streams(self, selected_stream_position, tops, bottoms):
         """
         :param selected_stream_position: the selected stream's position in the state
         :param tops top stream (Stream Class object)
+        :param bottoms bottoms stream (Stream Class object)
         """
-        self.streams[np.where(self.stream_state_mapper == selected_stream_position)[0][0]] = tops
+        self.streams[selected_stream_position] = tops
         self.streams.append(bottoms)
 
     def update_state(self, selected_stream_position, tops, bottoms):
         self.update_streams(selected_stream_position, tops, bottoms)
-        return self.create_state()
+        self.create_state()
+        return self.state
 
     @property
     def n_streams(self):
--- Env/DC_gym.py
+++ Env/DC_gym.py
@@ -1,37 +1,53 @@
 import numpy as np
-from Env.DC_class import SimulatorDC
-from Env.ClassDefinitions import Stream, State
+from Env2.DC_class import SimulatorDC
+from Env2.ClassDefinitions import Stream, State
+
+import comtypes.client
+import comtypes.gen
+from comtypes import COMError
+from comtypes.automation import VARIANT
+import array
+from multiprocessing import Process
+
+# tell comtypes to load type libs
+cofeTlb = ('{0D1006C7-6086-4838-89FC-FBDCC0E98780}', 1, 0)  # COFE type lib
+cofeTypes = comtypes.client.GetModule(cofeTlb)
+coTlb = ('{4A5E2E81-C093-11D4-9F1B-0010A4D198C2}', 1, 1)  # CAPE-OPEN v1.1 type lib
+coTypes = comtypes.client.GetModule(coTlb)
+
+
 
 class DiscreteGymDC(SimulatorDC):
     """
+    This is the gym for the ChemSep problem that inludes temperature and pressure drops
     First let's make an env with a big flat discrete action space
-    This env doesn't have temperature or pressure
     """
 
-    def __init__(self, doc_path, n_discretisations=5, **kwargs):
-        """
-        *args: can pass shuffle=True to Stream
-        """
+    def __init__(self, doc_path, n_discretisations=5):
         super().__init__(doc_path)
         self.n_discretisations = n_discretisations
-        self.feed = Stream(0, np.array(self.get_inlet_flowrates()))
-        self.n_components = self.feed.flows.size
+        feed_conditions = self.get_inlet_stream_conditions()
+        self.original_feed = Stream(0, feed_conditions["flows"],
+                           feed_conditions["temperature"],
+                           feed_conditions["pressure"]
+                           )
+        self.n_components = len(self.original_feed.flows)
         self.max_outlet_streams = self.n_components
 
-        # for now set the state as the stream table
-        self.stream_table = [self.feed]
+        self.stream_table = [self.original_feed]
 
-        self.State = State(self.feed, self.max_outlet_streams, **kwargs)
+        self.State = State(self.original_feed, self.max_outlet_streams)
 
         # contains a tuple of 3 (in, tops, bottoms) stream numbers describing the connections of streams & columns
         self.column_streams = []
 
         # Now configure action space
-        self.action_names = ['stream', 'number of stages', 'reflux ratio', 'reboil ratio']
-        self.n_stages_options = np.linspace(10, 50, n_discretisations).astype('int')
-        self.reflux_ratio_options = np.linspace(0.1, 5, n_discretisations)
-        self.reboil_ratio_options = np.linspace(0., 5, n_discretisations)
-        self.n_actions = self.max_outlet_streams * self.n_discretisations ** 3 + 1  # +1 for submit as final
+        self.action_names = ['stream', 'number of stages', 'reflux ratio', 'reboil ratio', 'pressure drop']
+        self.n_stages_options = np.linspace(20, 30, n_discretisations).astype('int')
+        self.reflux_ratio_options = np.linspace(1.5, 3, n_discretisations)
+        self.reboil_ratio_options = np.linspace(1.5, 3, n_discretisations)
+        self.pressure_drop_options = np.linspace(0, self.max_pressure/2, n_discretisations)  #  assumes max pressure drop of 50%
+        self.n_actions = self.max_outlet_streams * self.n_discretisations ** 4 + 1  # +1 for submit as final
 
     def step(self, action):
         if action == self.n_actions - 1:  # submit
@@ -41,20 +57,29 @@ class DiscreteGymDC(SimulatorDC):
             state = self.State.state
             return state, reward, done, info
         selected_stream_position = action % self.max_outlet_streams
-        self.set_inlet_flowrates(self.State.state[selected_stream_position])  # put the selected stream as the input to a new column
+
+        # put the selected stream (flows, temperature, pressure) as the input to a new column
+        selected_stream = self.State.streams[selected_stream_position]
+        self.set_inlet_stream(selected_stream.flows, selected_stream.temperature, selected_stream.pressure)
 
         n_stages = self.n_stages_options[action % (self.max_outlet_streams * self.n_discretisations) // self.max_outlet_streams]
-        reflux_ratio = self.reflux_ratio_options[action % (self.max_outlet_streams * self.n_discretisations**2) // (self.max_outlet_streams * self.n_discretisations)]
-        reboil_ratio = self.reboil_ratio_options[action // (self.max_outlet_streams * self.n_discretisations ** 2)]
+        reflux_ratio = self.reflux_ratio_options[action % (self.max_outlet_streams * self.n_discretisations**2) //
+                                                 (self.max_outlet_streams * self.n_discretisations)]
+        reboil_ratio = self.reboil_ratio_options[action % (self.max_outlet_streams * self.n_discretisations ** 3) //
+                                                 (self.max_outlet_streams * self.n_discretisations**2)]
+        pressure_drop = self.pressure_drop_options[action // (self.max_outlet_streams * self.n_discretisations ** 3)]
 
-        self.set_unit_inputs(n_stages, reflux_ratio, reboil_ratio)
+        self.set_unit_inputs(n_stages, reflux_ratio, reboil_ratio, pressure_drop)
         self.solve()
 
-        tops_flow, bottoms_flow, TAC, condenser_duty, reboiler_duty = self.get_outputs()
+        TAC, condenser_duty, reboiler_duty = self.get_outputs()
 
+        tops_info, bottoms_info = self.get_outlet_info()
+        tops_flow, tops_temperature, tops_pressure = tops_info
+        bottoms_flow, bottoms_temperature, bottoms_pressure = bottoms_info
         state = self.State.update_state(selected_stream_position,
-                                        Stream(self.State.n_streams, tops_flow),
-                                        Stream(self.State.n_streams+1, bottoms_flow))
+                                        Stream(self.State.n_streams, tops_flow, tops_temperature, tops_pressure),
+                                        Stream(self.State.n_streams+1, bottoms_flow, bottoms_temperature, bottoms_pressure))
         reward = self.reward_calculator(tops_flow, bottoms_flow, TAC, condenser_duty, reboiler_duty)
 
         if self.State.n_streams == self.max_outlet_streams:
@@ -62,6 +87,7 @@ class DiscreteGymDC(SimulatorDC):
         else:
             done = False
         info = {}
+        self.import_file()  # current workaround is to reset the file after each solve
         return state, reward, done, info
 
     def reward_calculator(self, tops_flow, bottoms_flow, TAC, condenser_duty, reboiler_duty):
@@ -73,20 +99,28 @@ class DiscreteGymDC(SimulatorDC):
 
     @property
     def legal_actions(self):
-        legal_selected_stream_position = self.State.stream_state_mapper[0:self.State.n_streams]
+        """
+        Illegal actions:
+         - Choose Null Stream in stream table
+         - Choose pressure drop that results in pressure below 0 # TODO update to below minimum pressure (e.g. 0.1 atm)
+        """
+        legal_selected_stream_position = range(0, self.State.n_streams)
         legal_actions = []
-        # TODO make this loop faster
+        # TODO make this loop faster - turn into matrices
         for action in range(self.n_actions - 1):
+            pressure_drop = self.pressure_drop_options[
+                action // (self.max_outlet_streams * self.n_discretisations ** 3)]
             for stream_position in legal_selected_stream_position:
                 if action % self.max_outlet_streams == stream_position:
-                    legal_actions.append(action)
-                    break
+                    stream_pressure = self.State.streams[stream_position].pressure
+                    if stream_pressure - pressure_drop >= 0:
+                        legal_actions.append(action)
+                        break
         legal_actions.append(self.n_actions-1)
         return legal_actions
 
     def reset(self):
         self.reset_flowsheet()
-        self.feed = Stream(0, np.array(self.get_inlet_flowrates()))
-        self.stream_table = [self.feed]
-        self.State = State(self.feed, self.max_outlet_streams)
+        self.stream_table = [self.original_feed]
+        self.State = State(self.original_feed, self.max_outlet_streams)
         self.column_streams = []
